896a92ff644c5bc821419c7261bd0625b401e218
Ali Moradi <Alimoradi0business@gmail.com>
2024-01-25 13:38:52 +0330

    some features added

--- src/components/FreelancerSocial/Social.js
+++ src/components/FreelancerSocial/Social.js
@@ -5,28 +5,26 @@ import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 function Social() {
   return (
     <div className="social-container">
-      <div className="container">
-        <div className="social-parent">
-          <div className="social-wrapper">
-            <a href="#">
-              <FaFacebook color="#fafafa" size="16px" />
-            </a>
-          </div>
-          <div className="social-wrapper">
-            <a href="#">
-              <FaTwitter color="#fafafa" size="16px" />
-            </a>
-          </div>
-          <div className="social-wrapper">
-            <a href="#">
-              <FaGithub color="#fafafa" size="16px" />
-            </a>
-          </div>
-          <div className="social-wrapper">
-            <a href="#">
-              <FaLinkedin color="#fafafa" size="16px" />
-            </a>
-          </div>
+      <div className="social-parent">
+        <div className="social-wrapper">
+          <a href="#" title="Facebook">
+            <FaFacebook color="#fafafa" size="16px" />
+          </a>
+        </div>
+        <div className="social-wrapper">
+          <a href="#" title="Twitter">
+            <FaTwitter color="#fafafa" size="16px" />
+          </a>
+        </div>
+        <div className="social-wrapper">
+          <a href="#" title="Github">
+            <FaGithub color="#fafafa" size="16px" />
+          </a>
+        </div>
+        <div className="social-wrapper">
+          <a href="#" title="LinkedIn">
+            <FaLinkedin color="#fafafa" size="16px" />
+          </a>
         </div>
       </div>
     </div>
