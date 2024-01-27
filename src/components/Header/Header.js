9c7e0ea7ecae9657b2f67cf77a009e296ede365c
Ali Moradi <Alimoradi0business@gmail.com>
2024-01-27 22:53:54 +0330

    mobile hamburger menue development completed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { FaFacebook, FaTwitter, FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Header() {
   return (
@@ -13,7 +14,7 @@ function Header() {
               </a>
             </h1>
           </div>
-          <div>
+          <div className="header-content-wrapper">
             <ul className="header-list-wrapper">
               <li>
                 <a href="#" className="active-page">
@@ -39,14 +40,53 @@ function Header() {
                 <a href="#">ارتباط با من</a>
               </li>
             </ul>
+            <ul className="mobile-header-list-wrapper">
+              <li>
+                <a href="#" className="active-page">
+                  خانه
+                </a>
+              </li>
+              <li>
+                <a href="#">درباره من</a>
+              </li>
+              <li>
+                <a href="#">خدمات</a>
+              </li>
+              <li>
+                <a href="#">نمونه کارها</a>
+              </li>
+              <li>
+                <a href="#">نظرها</a>
+              </li>
+              <li>
+                <a href="#">بلاگ</a>
+              </li>
+              <li>
+                <a href="#">ارتباط با من</a>
+              </li>
+              <div className="mobile-header-list-social-icons-wrapper">
+                <FaFacebook color="#fafafa"></FaFacebook>
+                <FaTwitter color="#fafafa"></FaTwitter>
+                <FaGithub color="#fafafa"></FaGithub>
+                <FaLinkedin color="#fafafa"></FaLinkedin>
+              </div>
+            </ul>
             <div className="menu-icon-wrapper">
-              <span className="menu-icon"></span>
+              <span onClick={hamburgerMenu} className="menu-icon"></span>
             </div>
           </div>
         </div>
       </div>
     </header>
   );
+}
+
+function hamburgerMenu(event) {
+  event.preventDefault();
+
+  document
+    .querySelector(`.mobile-header-list-wrapper`)
+    .classList.toggle(`active`);
 }
 
 export default Header;
